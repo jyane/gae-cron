33208181b8a3033fd0b24617b42b61d8a71756e9
Shohei Kamimori <jyane.kstm@gmail.com>
2018-04-17 13:01:44 +0900

    start to replace go

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	http.HandleFunc("/", handle)
+	appengine.Main()
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
