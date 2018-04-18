40aa559dc8f683ab7683080753a76781e04bbabe
Shohei Kamimori <jyane.kstm@gmail.com>
2018-04-18 22:18:23 +0900

    wip

--- main.go
+++ main.go
@@ -1,18 +1,50 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"cloud.google.com/go/pubsub"
 	"google.golang.org/appengine"
 )
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, world!")
+func publish(client *pubsub.Client, topic, msg string) error {
+	ctx := context.Background()
+	t := client.Topic(topic)
+	result := t.Publish(ctx, &pubsub.Message{
+		Data: []byte(msg),
+	})
+
+	id, err := result.Get(ctx)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Published a message; msg ID: %v", id)
+	return nil
 }
 
 func main() {
-	http.HandleFunc("/", handle)
+	ctx := context.Background()
+	proj := os.Getenv("PROJECT_ID")
+
+	_, err := pubsub.NewClient(ctx, proj)
+	if err != nil {
+		log.Fatalf("Could not create pubsub client: %v", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello")
+	})
+
+	http.HandleFunc("/publish", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		publish(client, "minutely-tick", "")
+		fmt.Fprintln(w, path)
+	})
+
 	appengine.Main()
 }
