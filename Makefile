bcf526aff0ae24eaaa8c18adf491c04c947cb5c7
Shohei Kamimori <jyane.kstm@gmail.com>
2018-04-23 01:37:55 +0900

    done

--- Makefile
+++ Makefile
@@ -1,15 +1,24 @@
+all: \
+	clean \
+	init \
+	build \
+	test
+
 build:
 	go build .
 
 clean:
 	go clean .
 
-run:
-	dev_appserver.py app.yaml
-
 init:
 	go get -u google.golang.org/appengine/...
 	go get -u cloud.google.com/go/...
+
+run:
+	dev_appserver.py app.yaml
+
+test:
+	go test .
 
 .PHONY: \
 	build \
--- main.go
+++ main.go
@@ -3,40 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"cloud.google.com/go/pubsub"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
-func publish(ctx context.Context, client *pubsub.Client, topic, msg string) (id string, err error) {
-	t := client.Topic(topic)
-	result := t.Publish(ctx, &pubsub.Message{
-		Data: []byte(msg),
+func publish(ctx context.Context, topic Topic) (string, error) {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {
+		log.Errorf(ctx, "Missing project id")
+		return "", fmt.Errorf("Missing project id")
+	}
+	client, err := pubsub.NewClient(ctx, os.Getenv(projectId))
+	if err != nil {
+		log.Errorf(ctx, "Failed to create the pubsub client %v", err)
+		return "", err
+	}
+	result := client.Topic(string(topic)).Publish(ctx, &pubsub.Message{
+		Data: []byte(""),
 	})
-	return result.Get(ctx)
+	id, err := result.Get(ctx)
+	if err != nil {
+		log.Errorf(ctx, "Failed to publish a message %v", err)
+		return "", err
+	}
+	return id, nil
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "ok")
+	fmt.Fprint(w, "ok")
 }
 
 func publishHandler(w http.ResponseWriter, r *http.Request) {
 	if topic, found := strToTopic[r.URL.Query().Get("topic")]; found {
 		ctx := appengine.NewContext(r)
-		client, err := pubsub.NewClient(ctx, os.Getenv("PROJECT_ID"))
-		if err != nil {
-			log.Fatalf("Could not create pubsub client: %v", err)
+		if id, err := publish(ctx, topic); err != nil {
+			http.Error(w, "internal", http.StatusInternalServerError)
+		} else {
+			fmt.Fprintf(w, "published: %v", id)
 		}
-		id, err := publish(ctx, client, string(topic), "a")
-		if err != nil {
-			log.Fatalf("Could not publish: %v", err)
-		}
-		fmt.Fprintf(w, "hello %v", id)
 	} else {
-		fmt.Fprintf(w, "bad topic")
+		http.Error(w, "bad topic", http.StatusBadRequest)
 	}
 }
 
