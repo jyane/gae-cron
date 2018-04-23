42d1d60375d4d6e033a09b7a3b84f357fb12f5ff
Shohei Kamimori <jyane.kstm@gmail.com>
2018-04-24 00:14:58 +0900

    start to replace python with go (#1)

---
+++ Makefile
@@ -0,0 +1,38 @@
+build:
+	go build .
+
+clean:
+	go clean .
+
+ci: \
+	fmt \
+	clean \
+	init \
+	build \
+	test
+
+deploy:
+	gcloud app deploy app.yaml \cron.yaml
+
+fmt:
+	go fmt .
+
+init:
+	go get -u google.golang.org/appengine/...
+	go get -u cloud.google.com/go/...
+
+run:
+	dev_appserver.py app.yaml
+
+test:
+	go test .
+
+.PHONY: \
+	build \
+	clean \
+	ci \
+	deploy \
+	fmt \
+	init \
+	run \
+	test \
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
+)
+
+func publish(ctx context.Context, topic Topic) (string, error) {
+	projectId := os.Getenv("PROJECT_ID")
+	if projectId == "" {
+		log.Errorf(ctx, "Missing project id")
+		return "", fmt.Errorf("Missing project id")
+	}
+	client, err := pubsub.NewClient(ctx, os.Getenv(projectId))
+	if err != nil {
+		log.Errorf(ctx, "Failed to create the pubsub client %v", err)
+		return "", err
+	}
+	result := client.TopicInProject(string(topic), projectId).Publish(ctx, &pubsub.Message{
+		Data: []byte("tick"),
+	})
+	id, err := result.Get(ctx)
+	if err != nil {
+		log.Errorf(ctx, "Failed to publish a message %v", err)
+		return "", err
+	}
+	return id, nil
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+func publishHandler(w http.ResponseWriter, r *http.Request) {
+	if topic, found := strToTopic[r.URL.Query().Get("topic")]; found {
+		ctx := appengine.NewContext(r)
+		if id, err := publish(ctx, topic); err != nil {
+			http.Error(w, "internal", http.StatusInternalServerError)
+		} else {
+			fmt.Fprintf(w, "published: %v", id)
+		}
+	} else {
+		http.Error(w, "bad topic", http.StatusBadRequest)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/publish", publishHandler)
+	appengine.Main()
+}
